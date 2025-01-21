18d6e93896e18167130f3c95c61e016f1e7a6fa7
Zimmermann Gyula <graiondilach@hotmail.com>
2025-01-21 17:30:06 +0100

    Implement sortable digimons and shift evoconditions to a getter.

--- Digimon.cs
+++ Digimon.cs
@@ -1,24 +1,45 @@
 ﻿namespace Cyber_Sleuth_Mod_Evolution_Analyzer
 {
-    class Digimon
+    class Digimon : IComparable
     {
         public readonly string Name;
         public readonly int Level;
-        public readonly List<Tuple<int, string>> EvoConditions;
+        readonly List<Tuple<int, string>> evoConditions;
+
+        public List<Tuple<int, string>> EvoConditions { get { return evoConditions; } }
 
         public Digimon(string name, int level, List<Tuple<int, string>> evoConditions)
         {
             Name = name;
             Level = level;
-            EvoConditions = evoConditions != null
-                ? evoConditions
-                : [];
+            this.evoConditions = evoConditions ?? ([]);
         }
         public Digimon()
         {
             Name = "TEMP";
             Level = -1;
-            EvoConditions = [];
+            evoConditions = [];
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+
+            Digimon digimon = (obj as Digimon)!;
+            if (digimon.Level < Level)
+            {
+                return -1;
+            }
+
+            if (digimon.Level > Level)
+            {
+                return 1;
+            }
+
+            return (string.Compare(Name, digimon.Name));
         }
     }
 }
